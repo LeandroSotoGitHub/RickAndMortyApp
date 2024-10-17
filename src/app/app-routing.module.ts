aaff6f2c2c8d7c391e2e259982d21ec57c55437d
Leandro Soto <soto.lean@gmail.com>
2024-10-17 14:34:31 -0300

    correcion de errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharacterListComponent } from '../app/pages/main/character-list/character-list.component';
-import { CharacterDetailComponent } from './pages/main/character-detail/character-detail.component';
 import { Error404Component } from './pages/error404/error404.component';
-import { RegisterComponent } from './pages/auth/register/register.component';
-import { LoginComponent } from './pages/auth/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
@@ -13,6 +9,7 @@ const routes: Routes = [
     loadChildren: () => import('../app/pages/main/main.module').then (m => m.MainModule),
     canActivate: [AuthGuard]
   },
+  
   {
     path: "login", 
     loadChildren: () => import('../app/pages/auth/auth.module').then (m => m.AuthModule)
